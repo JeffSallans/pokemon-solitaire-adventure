4abd201ac0112eba6bc9c25677958ebf95d6fb72
Jeff Sallans <jeffsallans@gmail.com>
2023-09-04 00:50:08 -0400

    Adjust something to try to get netlify working

--- src/routes/solitaire/solitaire-pack.ts
+++ src/routes/solitaire/solitaire-pack.ts
@@ -1,5 +1,5 @@
 import { PokemonTCG } from 'pokemon-tcg-sdk-typescript';
-import * as _ from "lodash";
+import { shuffle, take } from "lodash";
 
 /**
  * Generates a 11 card pokemon card pack for the given set name
@@ -9,12 +9,12 @@ import * as _ from "lodash";
  */
 export async function generatePack(setName: string): Promise<PokemonTCG.Card[]> {
 	const setCommons = (await PokemonTCG.findCardsByQueries({q: `set.id:${setName} rarity:Common`}));
-	const selectedCommons = _.take(_.shuffle(setCommons), 7);
+	const selectedCommons = take(shuffle(setCommons), 7);
 
 	const setUncommons = (await PokemonTCG.findCardsByQueries({q: `set.id:${setName} rarity:Uncommon`}));
-	const selectedUncommons = _.take(_.shuffle(setUncommons), 3);
+	const selectedUncommons = take(shuffle(setUncommons), 3);
 
 	const setRare = (await PokemonTCG.findCardsByQueries({q: `set.id:${setName} rarity:Rare*`}));
-	const selectedRare = _.take(_.shuffle(setRare), 1);
+	const selectedRare = take(shuffle(setRare), 1);
 	return [...selectedCommons, ...selectedUncommons, ...selectedRare];
 }
